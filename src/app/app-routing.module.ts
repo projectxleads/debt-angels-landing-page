bd7e0ccb1a3aa8d05db7077dc381042313157a4b
Michael <mihendricks1@gmail.com>
2019-06-09 19:39:37 +0200

    exclude module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: 'welcome', component: WelcomeComponent },
   { path: 'home', component: HomeComponent },
   { path: '**', component: NotFoundComponent }
 
