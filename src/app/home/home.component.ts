b2c849cc1901dd509b06732de55f65343483c12c
Michael <mihendricks1@gmail.com>
2019-06-20 17:54:10 +0200

    Work on validation on form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,13 +19,13 @@ export class HomeComponent implements OnInit {
     this.registrationFormGroup = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       firstName: new FormControl('', [Validators.required, Validators.minLength(2), , Validators.maxLength(50)]),
-      middleName: new FormControl('', [Validators.minLength(2), , Validators.maxLength(50)]),
       lastName: new FormControl('', [Validators.required, Validators.minLength(2), , Validators.maxLength(50)]),
       phoneNumber: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10),
               Validators.pattern(/^0(6|7|8){1}[0-9]{1}[0-9]{7}$/)]),
       alternativePhoneNumber: new FormControl('', [Validators.minLength(10), Validators.maxLength(10),
               Validators.pattern(/^0(6|7|8){1}[0-9]{1}[0-9]{7}$/)]),
-      methodOfCommunication: new FormControl('', [Validators.required])
+      methodOfCommunication: new FormControl('', []),
+      termsAndConditions: new FormControl('', [Validators.pattern("true"), Validators.required])
     });
   }
 
@@ -34,7 +34,7 @@ export class HomeComponent implements OnInit {
   }
 
   onSubmit(formValue) {
-
+    console.log(formValue);
   }
 
 }
